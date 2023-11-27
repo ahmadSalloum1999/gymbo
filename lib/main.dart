376d62fc1194ed83f6e9a2f579958094a74b363f
Ghandii Mahmoud <ghandiimahmoud@gmail.com>
2023-11-27 23:35:57 +0200

    My part of the App

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:workout_app/Screens/videoplayer.dart';
+
+
+import 'package:workout_app/Screens/welcome_view.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title:'workout',
+      home: WelcomeView(),
+    );
+  }
+}
